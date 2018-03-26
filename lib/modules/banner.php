cd51c29ab5dbb1fb594534c7db3d2102d9f1996f
duvid9320 <duvid9320@gmail.com>
2018-03-25 21:32:14 -0600

    Rebuild

--- lib/modules/banner.php
+++ lib/modules/banner.php
@@ -1,30 +1,5 @@
-<!--
-<div class="slider">
-    <div class="row">
-        <div class="col-md-12">
-            <ul class="ban">
-                <li><img src="images/a1.jpg" alt="a1"></li>
-                <li><img src="images/a2.jpg" alt="a2"></li>
-                <li><img src="images/a3.jpg" alt="a3"></li>
-                <li><img src="images/a4.jpg" alt="a4"></li>
-            </ul>
-            <div class="main-text text-center mono-italic-16">
-                <h1 style="font-size:12vw;" >
-                    DeliRoll
-                </h1>
-                <h1 style="font-size:4vw;" class="Letrero">
-                    Ven y disfruta de nuestras promociones
-                </h1>
-                <h1 style="font-size:3vw;" class="Letrero">
-                    o pide a domicilio.
-                </h1>
-            </div>
-        </div>
-    </div>
-</div>-->
 
-
-<div class="carousel slide " data-ride="carousel" data-interval="2000" >
+<div class="carousel slide " data-ride="carousel" data-interval="2000" data-pause="false" >
     <div class="carousel-inner">
         <div class="carousel-item active">
             <img class="d-block w-100 " src="images/a1.jpg" alt="First slide">
