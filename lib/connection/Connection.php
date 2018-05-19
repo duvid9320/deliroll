3c888d75bd1fe379a8915f64f74ffb7d47f4c81a
duvid9320 <duvid9320@gmail.com>
2018-05-19 16:32:38 -0500

    removing debug message

--- lib/connection/Connection.php
+++ lib/connection/Connection.php
@@ -13,9 +13,9 @@
  */
 class Connection {
 
-    private static $dns = 'mysql:dbname=daverdz_deliroll;host=65.19.141.67'; //65.19.141.67
-    private static $user = 'daverdz_deliroll';
-    private static $password = 'delirollitz';
+    private static $dns = 'mysql:dbname=daverdz_deliroll;host=127.0.0.1'; //65.19.141.67
+    private static $user = 'root'; //daverdz_deliroll
+    private static $password = 'root'; //delirollitz
     private static $connection;
     private $pdo;
     private $transaction;
