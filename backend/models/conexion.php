b1d140ba181c8769a90cd806b817e1d400d2c069
duvid9320 <duvid9320@gmail.com>
2018-06-11 23:37:10 -0500

    Update connection
    new user

--- backend/models/conexion.php
+++ backend/models/conexion.php
@@ -4,7 +4,7 @@ class Conexion{
 
 	public static function conectar(){
 
-		$link = new PDO("mysql:host=65.19.141.67;dbname=itz_deliroll","itz_admin","admindeliroll");
+		$link = new PDO("mysql:host=65.19.141.67;dbname=itz_deliroll","itz_admin2","admindeliroll");
 		return $link;
 
 	}
