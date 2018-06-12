7ad2ae63b61868367f6054257ed5b2b6b2de39f5
Dave <Dave@192.168.0.6>
2018-06-11 22:20:54 -0500

    Correción de llamada a funciones estáticas cuando no lo son.

--- backend/models/conexion.php
+++ backend/models/conexion.php
@@ -2,7 +2,7 @@
 
 class Conexion{
 
-	public function conectar(){
+	public static function conectar(){
 
 		$link = new PDO("mysql:host=65.19.141.67;dbname=itz_deliroll","itz_admin","admindeliroll");
 		return $link;
